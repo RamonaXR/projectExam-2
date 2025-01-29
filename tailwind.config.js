a2d410bb71a59a5e7dbc7e11f590ee05f42df52f
RamonaXR <Ramonaelise@gmail.com>
2025-01-29 16:36:27 +0100

    store zustand, header component and logo import

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/**/*.{js,jsx}'],
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#B7E2C4", // Soft green
+        secondary: "#F6BE6F", // Warm orange
+        button: "#F67878", // Button color
+      },
+    },
   },
   plugins: [],
 };
