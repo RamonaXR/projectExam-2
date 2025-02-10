f8ebe6c128245ce6b66fc01ebf8bc6d08c2811a6
RamonaXR <Ramonaelise@gmail.com>
2025-02-10 17:12:38 +0100

    searchbar, sort, venuecards, responsive, styling

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,17 +1,31 @@
+import { Link } from "react-router-dom";
 import VenueList from "../../components/VenueList";
 
 export default function Home() {
   return (
-    <main className="p-6 space-y-8">
-      <section className="text-center">
-        <h1 className="text-2xl font-bold">Search for Venue</h1>
-        <p className="text-gray-600">
-          Find the perfect venue for your next Holidaze!
-        </p>
+    <main className="space-y-10">
+      {/* Hero Section */}
+      <section className="relative">
+        <img
+          src="../../../public/img/heroholidaze.jpg"
+          alt="Hero image"
+          className="w-full object-cover h-60 sm:h-80 md:h-96"
+        />
+        <div className="absolute inset-0 bg-black bg-opacity-40 flex flex-col items-center justify-center text-center px-4">
+          <h1 className="text-white text-3xl sm:text-4xl md:text-5xl font-bold mb-4">
+            Book your next holidaze!
+          </h1>
+          <Link
+            to="/register"
+            className="bg-button text-white px-8 py-3 rounded-md text-lg hover:bg-blue-700 transition-colors"
+          >
+            Register
+          </Link>
+        </div>
       </section>
-
-      {/* Venue List */}
-      <VenueList />
+      <section>
+        <VenueList />
+      </section>
     </main>
   );
 }
