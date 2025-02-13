4d99a94854fd25144c4977b2b8444f4959fbf13b
RamonaXR <Ramonaelise@gmail.com>
2025-02-13 21:23:09 +0100

    adjustment hamburger menu, navbar, mobile nav, functionality loginstatus

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,15 @@ export default {
       fontFamily: {
         sans: ["Lunasima", "sans-serif"],
       },
+      keyframes: {
+        fadeIn: {
+          "0%": { opacity: 0 },
+          "100%": { opacity: 1 },
+        },
+      },
+      animation: {
+        fadeIn: "fadeIn 0.5s ease-out",
+      },
     },
   },
   plugins: [],
