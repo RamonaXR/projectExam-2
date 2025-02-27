7a5cf7ffcc5487f2469517ff04eadeee0f1d1723
RamonaXR <Ramonaelise@gmail.com>
2025-02-27 22:40:22 +0100

    Implemented useProfile hook to fetch user profile (with bookings and venues) using Tanstack Query object syntax and proper auth headers.

---
+++ src/hooks/useProfile.js
@@ -0,0 +1,33 @@
+import { useQuery } from "@tanstack/react-query";
+import { API_BASE_URL, API_KEY } from "../constants";
+import { useAuthStore } from "../store/authStore";
+
+export function useProfile(name, params = { _bookings: true, _venues: true }) {
+  const token = useAuthStore((state) => state.userProfile?.accessToken);
+  const queryKey = ["profile", name, params];
+  return useQuery({
+    queryKey,
+    queryFn: async () => {
+      const queryParams = new URLSearchParams(params);
+      const response = await fetch(
+        `${API_BASE_URL}/holidaze/profiles/${name}?${queryParams.toString()}`,
+        {
+          headers: {
+            "Content-Type": "application/json",
+            "X-Noroff-API-Key": API_KEY,
+            ...(token && { Authorization: `Bearer ${token}` }),
+          },
+        },
+      );
+      const data = await response.json();
+      if (!response.ok) {
+        throw new Error(
+          data.errors
+            ? data.errors.map((err) => err.message).join(", ")
+            : "Error fetching profile",
+        );
+      }
+      return data.data;
+    },
+  });
+}
