41991f3ff235cf3c61afe1c063f1ca5b5ca7dfe3
RamonaXR <Ramonaelise@gmail.com>
2025-03-02 21:35:27 +0100

    added jsdocs and final refacs

--- src/utils/fetchUtils.js
+++ src/utils/fetchUtils.js
@@ -1,3 +1,20 @@
+/**
+ * Fetches data from a given URL and returns the parsed JSON response.
+ *
+ * This function performs a GET request to the specified URL and attempts to parse
+ * the response as JSON. If the response is not successful (i.e., response.ok is false),
+ * it throws an error with a message constructed from the API's error messages or a generic
+ * server error message including the status code.
+ *
+ * @param {string} url - The URL to fetch data from.
+ * @returns {Promise<Object>} The JSON data retrieved from the URL.
+ * @throws {Error} Throws an error if the response status is not ok.
+ *
+ * @example
+ * fetchFn("https://api.example.com/data")
+ *   .then(data => console.log(data))
+ *   .catch(error => console.error(error));
+ */
 export async function fetchFn(url) {
   const response = await fetch(url);
   const data = await response.json();
