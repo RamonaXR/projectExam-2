dc93cd712ce0948b316dc37aca4370e7915a9e1b
RamonaXR <Ramonaelise@gmail.com>
2025-03-02 21:34:06 +0100

    added jsdocs and final refacs

--- src/hooks/useUpdateProfile.js
+++ src/hooks/useUpdateProfile.js
@@ -2,6 +2,23 @@ import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { API_BASE_URL, API_KEY } from "../constants";
 import { useAuthStore } from "../store/authStore";
 
+/**
+ * Custom hook to update a user's profile.
+ *
+ * This hook leverages react-query's useMutation to send a PUT request to update the user's profile.
+ * It uses the user's access token (if available) for authorization and sends the profile data as JSON.
+ * Upon successful update, it invalidates the query for the updated profile to ensure fresh data is fetched.
+ *
+ * @returns {object} The mutation object returned by useMutation, which includes properties such as mutate, isLoading, isError, etc.
+ *
+ * @example
+ * const { mutate: updateProfile } = useUpdateProfile();
+ * updateProfile({
+ *   name: "johnDoe",
+ *   bio: "Updated bio",
+ *   avatar: { url: "https://example.com/avatar.jpg", alt: "User avatar" }
+ * });
+ */
 export function useUpdateProfile() {
   const token = useAuthStore((state) => state.userProfile?.accessToken);
   const queryClient = useQueryClient();
