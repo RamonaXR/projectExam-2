82f1c185831344eb2e5a7c451af3dab5a352016d
RamonaXR <Ramonaelise@gmail.com>
2025-03-02 21:31:06 +0100

    added jsdocs and final refacs

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,3 +1,19 @@
+/**
+ * Button component renders a styled button element or a custom component.
+ *
+ * This component applies a default set of styles and allows additional styling
+ * through the `className` prop. It also supports rendering as a different element
+ * by using the `as` prop.
+ *
+ * @component
+ * @param {Object} props - Component properties.
+ * @param {React.ReactNode} props.children - The content to be displayed inside the button.
+ * @param {string} [props.className=""] - Additional CSS classes to apply.
+ * @param {string|React.ElementType} [props.as="button"] - The element type to render. Defaults to "button".
+ * @param {Object} [props.rest] - Additional props that are passed to the rendered component.
+ * @returns {JSX.Element} The rendered button component.
+ */
+
 export default function Button({
   children,
   className = "",
